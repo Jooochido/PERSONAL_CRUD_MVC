b06eacb852a17dad5006a68e61ecb17cd3326d4f
Jooochido <jooorami2608@gmail.com>
2022-08-14 11:59:08 -0600

    14/08/2022 Correciones

--- config/conexion.php
+++ config/conexion.php
@@ -4,7 +4,7 @@
 
         protected function Conexion(){
             try{
-                $conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=crud2","root","");
+                $conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=crud_mvc","root","");
                 return $conectar;
             }catch(Exception $e){
                 print "¡Error BD!: " . $e->getMessage() . "<br/>";
