e739bc370a11465ab8229766b924be1bd87a7b09
Jooochido <jooorami2608@gmail.com>
2022-08-14 12:20:51 -0600

    14/08/2022 Terminado

---
+++ docs/crud_mvc.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-08-2022 a las 20:18:48
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `crud_mvc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tm_producto`
+--
+
+CREATE TABLE `tm_producto` (
+  `prod_id` int(11) NOT NULL,
+  `prod_nom` varchar(150) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `prod_desc` varchar(255) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `fech_crea` datetime NOT NULL,
+  `fech_modi` datetime DEFAULT NULL,
+  `fech_elim` datetime DEFAULT NULL,
+  `est` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `tm_producto`
+--
+
+INSERT INTO `tm_producto` (`prod_id`, `prod_nom`, `prod_desc`, `fech_crea`, `fech_modi`, `fech_elim`, `est`) VALUES
+(1, 'Monitor', 'Ninguna', '2022-08-14 09:55:52', NULL, NULL, 1),
+(2, 'Teclado', 'Ninguna', '2022-08-14 09:55:52', NULL, NULL, 1),
+(3, 'Mouse', 'Ninguna', '2022-08-14 09:56:24', NULL, NULL, 1),
+(4, 'Cargador', 'Ninguna', '2022-08-14 11:59:48', '2022-08-14 12:15:36', NULL, 1),
+(5, 'Audífonos', 'Chidos', '2022-08-14 12:18:06', NULL, '2022-08-14 12:18:17', 0);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tm_producto`
+--
+ALTER TABLE `tm_producto`
+  ADD PRIMARY KEY (`prod_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tm_producto`
+--
+ALTER TABLE `tm_producto`
+  MODIFY `prod_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
